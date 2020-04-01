b81f1d127196335bc1cacc7a0f1839fd4b9dd05e
didier <didier@d-defrance.fr>
2020-04-01 16:53:01 +0200

    debut tri

--- poo/src/org/ipssi/p2/TestApp2.java
+++ poo/src/org/ipssi/p2/TestApp2.java
@@ -1,9 +1,25 @@
 package org.ipssi.p2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestApp2 {
 
 
 	public static void main(String[] args) {
+		//m1();
+		m2();
+	}
+	public static void m2() {
+		List<Integer> listeInt = new ArrayList<>();
+		listeInt.add(6); listeInt.add(3); listeInt.add(7); listeInt.add(2);
+		System.out.println(listeInt);
+		Collections.sort(listeInt);
+		System.out.println(listeInt);
+	}
+	
+	public static void m1() {
 		MyStack<String> pileChaines = new MyStack<>();
 		pileChaines.push("ch1");pileChaines.push("ch2");pileChaines.push("ch3");
 		System.out.println(pileChaines.pop());
