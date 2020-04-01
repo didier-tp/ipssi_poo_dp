4688f7ed7c9b2fdd30f516c625cf05f7c96ce496
didier <didier@d-defrance.fr>
2020-04-01 11:46:25 +0200

    suite

--- poo/src/org/ipssi/p1/Employe.java
+++ poo/src/org/ipssi/p1/Employe.java
@@ -1,5 +1,42 @@
 package org.ipssi.p1;
 
-public class Employe {
+public class Employe extends Personne {
+	
+	private Integer salaireFixe;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Employe heritant de " + super.toString()
+		       + " avec salairefixe=" + this.salaireFixe;
+	}
 
+	public Employe() {
+		super();//réappel un constructeur de la super classe
+	}
+
+	public Employe(String nom, String prenom, Double taille) {
+		super(nom, prenom, taille);
+	}
+	
+	public Employe(String nom, String prenom, Double taille,Integer salaireFixe) {
+		super(nom, prenom, taille);
+		this.salaireFixe = salaireFixe;
+	}
+
+	public Employe(Integer salaireFixe) {
+		super();
+		this.salaireFixe = salaireFixe;
+	}
+
+	public Integer getSalaireFixe() {
+		return salaireFixe;
+	}
+
+	public void setSalaireFixe(Integer salaireFixe) {
+		this.salaireFixe = salaireFixe;
+	}
+	
+	
 }
--- poo/src/org/ipssi/p1/TestApp.java
+++ poo/src/org/ipssi/p1/TestApp.java
@@ -1,8 +1,8 @@
 package org.ipssi.p1;
 
 public class TestApp {
-
-	public static void main(String[] args) {
+	
+	public static void m1() {
 		Personne p1 = null;
 		p1 = new Personne();
 		p1.setNom("toto");
@@ -12,6 +12,30 @@ public class TestApp {
 		p2.grandir(0.05);
 		System.out.println(p2.toString());
 		System.out.println(p2);
+		
+		Employe e1 = new Employe();
+		e1.setSalaireFixe(2000);
+		System.out.println("salaire de e1=" + e1.getSalaireFixe());
+		
+		Employe e2 = new Employe("Defrance","didier",1.82,2000);
+		e2.setNom("NouveauNom");
+		System.out.println(e2.toString());
+	}
+	
+	public static void m2() {
+	   Personne p =null; //référence sur une personne quelconque
+	   Personne p1= new Personne("Aire", "Axelle" , 1.68);
+	   Employe e1=new Employe("Bon","jean",1.76,2000);
+	   p=p1;
+	   System.out.println(p.toString());
+	   p=e1;
+	   System.out.println(p.toString());//polymorphisme sur .toString()
+	}
+
+	public static void main(String[] args) {
+		
+		//m1();
+		m2();
 	}
 
 }
